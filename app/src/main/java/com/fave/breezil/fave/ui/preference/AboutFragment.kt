1a10e35693117cb2b5320070b7baa4f5d5e300c2
Kola <kolajubril@gmail.com>
2020-07-09 03:01:46 +0100

    Added tabview layouts

--- app/src/main/java/com/fave/breezil/fave/ui/preference/AboutFragment.kt
+++ app/src/main/java/com/fave/breezil/fave/ui/preference/AboutFragment.kt
@@ -2,13 +2,11 @@ package com.fave.breezil.fave.ui.preference
 
 import android.os.Bundle
 import android.view.Gravity
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.fave.breezil.fave.R
-import com.fave.breezil.fave.databinding.ActivityAboutBinding
 import com.fave.breezil.fave.databinding.FragmentAboutBinding
 import dagger.android.support.DaggerFragment
 import mehdi.sakout.aboutpage.AboutPage
